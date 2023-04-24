a515edf589ce42546da82065848143ba75b21920
Mark Lodato <marklodato0@gmail.com>
2023-04-24 15:38:10 -0400

    Project 1 - a simple in-memory key value store
    ## This Commit
    
    Follows [project 1][project1] to create an in-memory key value store
    that passes some skeleton tests.
    
    [project1]: https://github.com/pingcap/talent-plan/blob/master/courses/rust/projects/project-1/README.md#pna-rust-project-1-the-rust-toolbox

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! I'm not 100% sure what goes here. Either a KV Store or utilities for building one or something.
+//! I'll learn more as we go, I'm sure.
+//!
+//! Built following https://github.com/pingcap/talent-plan/blob/master/courses/rust/README.md.
+
+mod kv_store;
+
+pub use kv_store::KvStore;
