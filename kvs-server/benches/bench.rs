e77fcaa28148c589920358c975d10457ed3405b2
Mark Lodato <marklodato0@gmail.com>
2024-01-28 19:07:38 -0500

    Benchmark HashMaps too

--- kvs-server/benches/bench.rs
+++ kvs-server/benches/bench.rs
@@ -1,3 +1,4 @@
+use std::hint::black_box;
 use std::time::Instant;
 
 use criterion::{criterion_group, criterion_main, Criterion};
@@ -18,12 +19,12 @@ fn bench_writes(c: &mut Criterion) {
     let mut group = c.benchmark_group("write");
 
     let mut rng = get_rng();
-    let cmds: Vec<_> = (0..100)
+    let key_values: Vec<_> = (0..100)
         .map(|_| {
             let len = rng.gen_range(1..=100_000);
             let key = Alphanumeric.sample_string(&mut rng, len as usize);
             let value = Alphanumeric.sample_string(&mut rng, len as usize);
-            Cmd::Set(key.into(), value.into())
+            (key, value)
         })
         .collect();
 
@@ -36,8 +37,9 @@ fn bench_writes(c: &mut Criterion) {
 
             let start = Instant::now();
             for _i in 0..iters {
-                for cmd in &cmds {
-                    if let Response::Err(e) = kvs.handle_cmd(cmd.clone()) {
+                for (key, value) in &key_values {
+                    let cmd = Cmd::Set(key.into(), value.into());
+                    if let Response::Err(e) = kvs.handle_cmd(cmd) {
                         panic!("Unexpected error: {e:?}")
                     }
                 }
@@ -53,10 +55,39 @@ fn bench_writes(c: &mut Criterion) {
 
             let start = Instant::now();
             for _i in 0..iters {
-                for cmd in &cmds {
-                    if let Response::Err(e) = kvs.handle_cmd(cmd.clone()) {
+                for (key, value) in &key_values {
+                    let cmd = Cmd::Set(key.into(), value.into());
+                    if let Response::Err(e) = kvs.handle_cmd(cmd) {
                         panic!("Unexpected error: {e:?}")
                     }
+                }
+            }
+            start.elapsed()
+        })
+    });
+    group.bench_function("std::HashMap", |b| {
+        b.iter_custom(|iters| {
+            let mut kvs = std::collections::HashMap::new();
+
+            let start = Instant::now();
+            for _i in 0..iters {
+                for (key, value) in &key_values {
+                    kvs.insert(key.to_owned(), value);
+                    kvs = black_box(kvs);
+                }
+            }
+            start.elapsed()
+        })
+    });
+    group.bench_function("hashbrown::HashMap", |b| {
+        b.iter_custom(|iters| {
+            let mut kvs = hashbrown::HashMap::new();
+
+            let start = Instant::now();
+            for _i in 0..iters {
+                for (key, value) in &key_values {
+                    kvs.insert(key.to_owned(), value);
+                    kvs = black_box(kvs);
                 }
             }
             start.elapsed()
@@ -70,22 +101,16 @@ fn bench_reads(c: &mut Criterion) {
     let mut group = c.benchmark_group("read");
 
     let mut rng = get_rng();
-    let sets: Vec<_> = (0..100)
+    let key_values: Vec<_> = (0..100)
         .map(|_| {
             let len = rng.gen_range(1..=100_000);
             let key = Alphanumeric.sample_string(&mut rng, len as usize);
             let value = Alphanumeric.sample_string(&mut rng, len as usize);
-            Cmd::Set(key.into(), value.into())
+            (key, value)
         })
         .collect();
-    let mut gets: Vec<_> = sets
-        .iter()
-        .map(|cmd| match cmd {
-            Cmd::Set(k, _) => Cmd::Get(k.clone()),
-            _ => unreachable!(),
-        })
-        .collect();
-    gets.shuffle(&mut rng);
+    let mut keys: Vec<_> = key_values.iter().map(|(key, _)| key.to_owned()).collect();
+    keys.shuffle(&mut rng);
 
     group.bench_function("kvs", |b| {
         b.iter_custom(|iters| {
@@ -93,16 +118,18 @@ fn bench_reads(c: &mut Criterion) {
             let dir = TempDir::new().unwrap();
             let mut kvs = KvsServer::open(Some("kvs"), dir.path()).unwrap();
 
-            for cmd in &sets {
-                if let Response::Err(e) = kvs.handle_cmd(cmd.clone()) {
+            for (key, value) in &key_values {
+                let cmd = Cmd::Set(key.into(), value.into());
+                if let Response::Err(e) = kvs.handle_cmd(cmd) {
                     panic!("Unexpected error: {e:?}")
                 }
             }
 
             let start = Instant::now();
             for _i in 0..iters {
-                for get in &gets {
-                    if let Response::Err(e) = kvs.handle_cmd(get.clone()) {
+                for key in &keys {
+                    let get = Cmd::Get(key.into());
+                    if let Response::Err(e) = kvs.handle_cmd(get) {
                         panic!("Unexpected error: {e:?}")
                     }
                 }
@@ -116,16 +143,18 @@ fn bench_reads(c: &mut Criterion) {
             let dir = TempDir::new().unwrap();
             let mut kvs = KvsServer::open(Some("sled"), dir.path()).unwrap();
 
-            for cmd in &sets {
-                if let Response::Err(e) = kvs.handle_cmd(cmd.clone()) {
+            for (key, value) in &key_values {
+                let cmd = Cmd::Set(key.into(), value.into());
+                if let Response::Err(e) = kvs.handle_cmd(cmd) {
                     panic!("Unexpected error: {e:?}")
                 }
             }
 
             let start = Instant::now();
             for _i in 0..iters {
-                for get in &gets {
-                    if let Response::Err(e) = kvs.handle_cmd(get.clone()) {
+                for key in &keys {
+                    let get = Cmd::Get(key.into());
+                    if let Response::Err(e) = kvs.handle_cmd(get) {
                         panic!("Unexpected error: {e:?}")
                     }
                 }
