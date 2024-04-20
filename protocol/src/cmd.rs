e55276a48dcb3ab169c327d21610e73b171c5d78
Mark Lodato <marklodato0@gmail.com>
2024-04-20 14:22:33 -0400

    Use simpler binary format in protocol
    ## This Commit
    
    Leverages the binary format suggested by Chris Rice based on [this
    paper][0].
    
    ## Why?
    
    It's a much easier to parse format, even though it isn't human readable.
    
    [0]: https://github.com/basho/bitcask/blob/develop/doc/bitcask-intro.pdf

--- protocol/src/cmd.rs
+++ protocol/src/cmd.rs
@@ -12,12 +12,10 @@ use anyhow::{Error, Result};
 // Implementation details:
 //
 // The current protocol is:
-//   1. `Set` commands are encoded as "{},{},{}{}" where the first bracket is the key length in
-//      ASCII, the second is the value length in ASCII, the third is the key, and the fourth is the
-//      value.
-//   2. `Get` commands are encoded as "g{},{}" where the first bracket is the key length in
-//      ASCII and the second is the key.
-//   3. `Rm` commands are encoded the same as `Get` commands, except with the tag `r`.
+//   1. All commands start with 4 bytes for the key length and 8 bytes for the value length.
+//   2. `Get` commands always specify a value length of `GET_VALUE_LEN`. Similarly for `Rm`.
+//   3. Following this header, the key is stored.
+//   4. Finally, for `Set` commands, the value is stored.
 //
 // TODO Can we make these comments unnecessary with a descriptive trait?
 #[derive(Debug, PartialEq)]
@@ -30,58 +28,48 @@ pub enum Cmd<'a> {
     Rm(Cow<'a, str>),
 }
 
-const RM_BYTE: u8 = b'r';
-const GET_BYTE: u8 = b'g';
+const HEADER_KEY_BYTES: usize = 4;
+const HEADER_VALUE_BYTES: usize = 8;
+const HEADER_BYTES: usize = HEADER_KEY_BYTES + HEADER_VALUE_BYTES;
+
+const GET_VALUE_LEN: u64 = u64::MAX;
+const RM_VALUE_LEN: u64 = GET_VALUE_LEN - 1;
 
 impl<'a> Cmd<'a> {
     /// Parses a `Cmd` out of the provided byte buffer.
     pub fn parse(cmd: &'a [u8]) -> Result<Self> {
         debug!(?cmd, "Received command");
-        match cmd.first().copied() {
-            None => Err(Error::msg("Invalid empty command")),
-            Some(b @ GET_BYTE | b @ RM_BYTE) => {
-                let mut parts = cmd[1..].splitn(2, |b| *b == b',');
 
-                let key_part = parts.next().ok_or_else(|| Error::msg("Missing key len"))?;
-                let key_len = parse_ascii_len(key_part)?;
+        if cmd.len() < HEADER_BYTES {
+            return Err(Error::msg("Missing proper header"));
+        }
 
-                let rest = parts.next().ok_or_else(|| Error::msg("Missing data"))?;
-                let key = rest
-                    .get(..key_len)
-                    .ok_or_else(|| Error::msg("Not enough data for specified data lengths"))?;
+        let (header, rest) = cmd.split_at(HEADER_BYTES);
+        let (key_len, value_len) = header.split_at(HEADER_KEY_BYTES);
 
-                let key =
-                    std::str::from_utf8(key).map_err(|e| Error::new(e).context("Invalid key"))?;
+        let key_len = u32::from_be_bytes(key_len.try_into().expect("specified 4 bytes"));
+        let value_len = u64::from_be_bytes(value_len.try_into().expect("specified 8 bytes"));
 
-                if b == GET_BYTE {
-                    Ok(Self::Get(Cow::Borrowed(key)))
-                } else {
-                    Ok(Self::Rm(Cow::Borrowed(key)))
-                }
-            }
-            _ => {
-                let mut parts = cmd.splitn(3, |b| *b == b',');
+        debug!(?key_len, ?value_len, "Header lengths");
 
-                let key_part = parts.next().ok_or_else(|| Error::msg("Missing key len"))?;
-                let key_len = parse_ascii_len(key_part)?;
+        if rest.len() < key_len as usize {
+            return Err(Error::msg("Insufficient data for key"));
+        }
 
-                let value_part = parts
-                    .next()
-                    .ok_or_else(|| Error::msg("Missing value len"))?;
-                let value_len = parse_ascii_len(value_part)?;
+        let (key_bytes, value_bytes) = rest.split_at(key_len as usize);
+        let key =
+            std::str::from_utf8(key_bytes).map_err(|e| Error::new(e).context("Non-UTF8 key"))?;
 
-                let rest = parts.next().ok_or_else(|| Error::msg("Missing data"))?;
-                let rest = rest
-                    .get(..key_len + value_len)
-                    .ok_or_else(|| Error::msg("Not enough data for specified data lengths"))?;
-
-                let (key, value) = rest.split_at(key_len);
-                let key =
-                    std::str::from_utf8(key).map_err(|e| Error::new(e).context("Invalid key"))?;
-                let value = std::str::from_utf8(value)
-                    .map_err(|e| Error::new(e).context("Invalid value"))?;
-
-                Ok(Self::Set(Cow::Borrowed(key), Cow::Borrowed(value)))
+        match value_len {
+            GET_VALUE_LEN => Ok(Self::Get(key.into())),
+            RM_VALUE_LEN => Ok(Self::Rm(key.into())),
+            value_len => {
+                let value_bytes = value_bytes
+                    .get(..value_len as usize)
+                    .ok_or_else(|| Error::msg("Insufficient data for value"))?;
+                let value = std::str::from_utf8(value_bytes)
+                    .map_err(|e| Error::new(e).context("Non-UTF8 value"))?;
+                Ok(Self::Set(key.into(), value.into()))
             }
         }
     }
@@ -89,32 +77,26 @@ impl<'a> Cmd<'a> {
     /// Writes the `Cmd` into the provided writer.
     pub fn write<W: Write>(&self, w: &mut W) -> Result<()> {
         match self {
-            Self::Set(key, value) => write!(w, "{},{},{key}{value}", key.len(), value.len())?,
+            Self::Set(key, value) => {
+                // TODO is the any value in buffering these?
+                w.write_all(&(key.len() as u32).to_be_bytes())?;
+                w.write_all(&(value.len() as u64).to_be_bytes())?;
+                w.write_all(key.as_bytes())?;
+                w.write_all(value.as_bytes())?;
+            }
             Self::Get(key) => {
-                // TODO These are a bit unwieldy ... could they be improved?
-                w.write_all(&[GET_BYTE])?;
-                write!(w, "{},{key}", key.len())?;
+                w.write_all(&(key.len() as u32).to_be_bytes())?;
+                w.write_all(&GET_VALUE_LEN.to_be_bytes())?;
+                w.write_all(key.as_bytes())?;
             }
             Self::Rm(key) => {
-                w.write_all(&[RM_BYTE])?;
-                write!(w, "{},{key}", key.len())?;
+                w.write_all(&(key.len() as u32).to_be_bytes())?;
+                w.write_all(&RM_VALUE_LEN.to_be_bytes())?;
+                w.write_all(key.as_bytes())?;
             }
         }
         Ok(())
     }
-}
-
-fn parse_ascii_len(len: &[u8]) -> Result<usize> {
-    if len.len() >= 10 {
-        return Err(Error::msg("Too much data")); // ?
-    }
-    let mut l = 0;
-    let mut base = 1;
-    for &b in len.iter().rev() {
-        l += base * (b - b'0') as usize;
-        base *= 10;
-    }
-    Ok(l)
 }
 
 #[cfg(test)]
@@ -123,42 +105,72 @@ mod tests {
 
     #[test]
     fn set() {
-        let actual = Cmd::parse(b"3,6,foofoobar").unwrap();
+        let mut bytes = Vec::new();
+        bytes.extend(3u32.to_be_bytes());
+        bytes.extend(6u64.to_be_bytes());
+        bytes.extend(b"foofoobar");
+
+        let actual = Cmd::parse(&bytes).unwrap();
         let expected = Cmd::Set("foo".into(), "foobar".into());
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn rm() {
-        let actual = Cmd::parse(b"r3,foo").unwrap();
+        let mut bytes = Vec::new();
+        bytes.extend(3u32.to_be_bytes());
+        bytes.extend(RM_VALUE_LEN.to_be_bytes());
+        bytes.extend(b"foo");
+
+        let actual = Cmd::parse(&bytes).unwrap();
         let expected = Cmd::Rm("foo".into());
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn get() {
-        let actual = Cmd::parse(b"g3,foo").unwrap();
+        let mut bytes = Vec::new();
+        bytes.extend(3u32.to_be_bytes());
+        bytes.extend(GET_VALUE_LEN.to_be_bytes());
+        bytes.extend(b"foo");
+
+        let actual = Cmd::parse(&bytes).unwrap();
         let expected = Cmd::Get("foo".into());
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn set_ignore_extra() {
-        let actual = Cmd::parse(b"3,6,foofoobar_ignoreme").unwrap();
+        let mut bytes = Vec::new();
+        bytes.extend(3u32.to_be_bytes());
+        bytes.extend(6u64.to_be_bytes());
+        bytes.extend(b"foofoobar_ignoreme");
+
+        let actual = Cmd::parse(&bytes).unwrap();
         let expected = Cmd::Set("foo".into(), "foobar".into());
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn rm_ignore_extra() {
-        let actual = Cmd::parse(b"r3,foo_ignoreme").unwrap();
+        let mut bytes = Vec::new();
+        bytes.extend(3u32.to_be_bytes());
+        bytes.extend(RM_VALUE_LEN.to_be_bytes());
+        bytes.extend(b"foo_ignoreme");
+
+        let actual = Cmd::parse(&bytes).unwrap();
         let expected = Cmd::Rm("foo".into());
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn get_ignore_extra() {
-        let actual = Cmd::parse(b"g3,foo_ignoreme").unwrap();
+        let mut bytes = Vec::new();
+        bytes.extend(3u32.to_be_bytes());
+        bytes.extend(GET_VALUE_LEN.to_be_bytes());
+        bytes.extend(b"foo_ignoreme");
+
+        let actual = Cmd::parse(&bytes).unwrap();
         let expected = Cmd::Get("foo".into());
         assert_eq!(actual, expected);
     }
@@ -200,24 +212,47 @@ mod tests {
         assert_eq!(Cmd::parse(&buf).unwrap(), proto);
     }
 
-    #[test]
-    fn len_doesnt_match() {
-        assert!(Cmd::parse(b"5,5,foolsbars").is_err());
-        assert!(Cmd::parse(b"g5,foo").is_err());
-        assert!(Cmd::parse(b"r5,foo").is_err());
-    }
+    mod len_check_tests {
+        use super::*;
 
-    #[test]
-    fn missing_commas() {
-        assert!(Cmd::parse(b"55,foolsbars").is_err());
-        assert!(Cmd::parse(b"5,5foolsbars").is_err());
-        assert!(Cmd::parse(b"g5foo").is_err());
-        assert!(Cmd::parse(b"r5foo").is_err());
-    }
+        #[test]
+        fn key_len_chec() {
+            let mut bytes = Vec::new();
+            bytes.extend(5u32.to_be_bytes());
+            bytes.extend(GET_VALUE_LEN.to_be_bytes());
+            bytes.extend(b"fo");
 
-    #[test]
-    fn excessive_data() {
-        assert!(Cmd::parse(b"g1000000000,lolnoway").is_err());
+            assert!(Cmd::parse(&bytes).is_err());
+        }
+
+        #[test]
+        fn set_checks_len() {
+            let mut bytes = Vec::new();
+            bytes.extend(5u32.to_be_bytes());
+            bytes.extend(5u64.to_be_bytes());
+            bytes.extend(b"foolsbars");
+
+            assert!(Cmd::parse(&bytes).is_err());
+        }
+
+        #[test]
+        fn rm_checks_len() {
+            let mut bytes = Vec::new();
+            bytes.extend(5u32.to_be_bytes());
+            bytes.extend(RM_VALUE_LEN.to_be_bytes());
+            bytes.extend(b"fool");
+
+            assert!(Cmd::parse(&bytes).is_err());
+        }
+        #[test]
+        fn get_checks_len() {
+            let mut bytes = Vec::new();
+            bytes.extend(5u32.to_be_bytes());
+            bytes.extend(GET_VALUE_LEN.to_be_bytes());
+            bytes.extend(b"fool");
+
+            assert!(Cmd::parse(&bytes).is_err());
+        }
     }
 
     #[test]
